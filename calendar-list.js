b01f733bcf208096474225d19ef369f5685fd12b
devanaclimgo <anaclimgo@gmail.com>
2024-10-26 18:51:46 -0300

    código base lista marcado e adicionado

--- calendar-list.js
+++ calendar-list.js
@@ -1,18 +1,27 @@
-// const taskInput = document.getElementById('taskInput')
-// const addBtn = document.getElementById('addBtn')
-// const taskList = document.getElementById('taskList')
-// const taskElement = document.getElementById('taskElement')
+const taskInput = document.getElementById('taskInput')
+const addBtn = document.getElementById('addBtn')
+const taskList = document.getElementById('taskList')
 
-// function listAdd() {
-//   addBtn.addEventListener('click', () => {
-//     const taskText = taskInput.value
-//     if (taskText !== '') {
-//       const text = document.createElement('p')
-//       li.textContent = taskText
-//       taskElement.appendChild(text)
-//       taskInput.value = ''
-//     }
-//   })
-// }
+  const action = addBtn.addEventListener('click', () => {
+    if (taskInput.value != '') {
+      const newTask = document.createElement('li')
 
-// listAdd()
+      const markBtn = document.createElement('button')
+      markBtn.innerHTML = `<svg width="20" height="20" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" fill="none"> <path stroke="red" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M29 16a13 13 0 11-26 0 13 13 0 0126 0h0z"/>
+    <path stroke="red" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.5 16l3.5 3.5 6-6"/>
+  </svg>`
+      markBtn.addEventListener('click', () => {
+        const svg = markBtn.querySelector('svg')
+        const paths = svg.querySelectorAll('path')
+        paths.forEach(path => {
+          path.style.stroke = 'green'
+        })
+      })
+
+      newTask.appendChild(markBtn)
+      newTask.appendChild(document.createTextNode(taskInput.value))
+
+      taskList.appendChild(newTask)
+      taskInput.value = ''
+    }
+  })
