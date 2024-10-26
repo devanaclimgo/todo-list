0d4b54859bf6cf9d76050d54868cc828b4a7ff23
devanaclimgo <anaclimgo@gmail.com>
2024-10-26 18:19:32 -0300

    escopo lista

---
+++ calendar-list.js
@@ -0,0 +1,18 @@
+// const taskInput = document.getElementById('taskInput')
+// const addBtn = document.getElementById('addBtn')
+// const taskList = document.getElementById('taskList')
+// const taskElement = document.getElementById('taskElement')
+
+// function listAdd() {
+//   addBtn.addEventListener('click', () => {
+//     const taskText = taskInput.value
+//     if (taskText !== '') {
+//       const text = document.createElement('p')
+//       li.textContent = taskText
+//       taskElement.appendChild(text)
+//       taskInput.value = ''
+//     }
+//   })
+// }
+
+// listAdd()
