8f8d7285890d3c638209dc558cfd191d51d03b10
devanaclimgo <anaclimgo@gmail.com>
2024-10-26 16:10:28 -0300

    quotes added

---
+++ script.js
@@ -0,0 +1,42 @@
+const quotes = [
+  {
+    "quote": "The only way most people recognize their limits is by trespassing on them.",
+    "source": "Tom Morris"
+  },
+  {
+    "quote": "Could we change our attitude, we should not only see life differently, but life itself would come to be different. Life would undergo a change of appearance because we ourselves had undergone a change in attitude.",
+    "source": "Katherine Mansfield "
+  },
+  {
+    "quote": "We do not need magic to transform our world. We carry all the power we need inside ourselves already. We have the power to imagine better.",
+    "source": "J. K. Rowling"
+  },
+  {
+    "quote": "It is impossible to go through life without trust: That is to be imprisoned in the worst cell of all, oneself.",
+    "source": "Graham Greene"
+  },
+  {
+    "quote": "He who asks is a fool for five minutes, but he who does not ask remains a fool forever.",
+    "source": "Chinese Proverb"
+  },
+  {
+    "quote": "The roots of true achievement lie in the will to become the best that you can become.",
+    "source": "Harold Taylor"
+  },
+  {
+    "quote": "A friend is a gift you give yourself.",
+    "source": "Robert Louis Stevenson"
+  },
+  {
+    "quote": "You can't stay in your corner of the Forest waiting for others to come to you. You have to go to them sometimes.",
+    "source": "Pooh's Little Instruction Book,"
+  },
+  {
+    "quote": "The Past is to be respected and acknoledged, but not to be worshiped. It is our future in which we will find our greatness.",
+    "source": "Pierre Trudeau"
+  },
+  {
+    "quote": "I don't believe that if you do good, good things will happen. Everything is completely accidental and random. Sometimes bad things happen to very good people and sometimes good things happen to bad people. But at least if you try to do good things, then you're spending your time doing something worthwhile.",
+    "source": "Hellen Mirren"
+  },
+]
